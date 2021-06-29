86fb534262cb40e022ba99002855f5f126f3c5cc
Saumyajit Chowdhury <77187489+smyjt@users.noreply.github.com>
2021-06-29 12:47:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,80 @@
+// Detecting button click
+
+var numberOfDrum = document.querySelectorAll(".drum").length;
+
+for( var i=0; i<numberOfDrum; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+    var buttonKey = this.innerHTML;
+    makeSound(buttonKey);
+    buttonAnimation(buttonKey);
+
+  });
+}
+
+// Detecting keyboard press
+
+document.addEventListener("keydown", function(event) {
+
+  makeSound(event.key);
+  buttonAnimation(event.key);
+
+});
+
+// Function to make sound corresponding to buttton clicked or key pressed
+
+function makeSound(key) {
+  switch(key) {
+
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "l":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    default: console.log(buttonCharacter);
+
+  }
+}
+
+// Function to create animation for button clicked or key pressed
+
+function buttonAnimation(key) {
+
+  var activeButton = document.querySelector("."+key);
+  activeButton.classList.add("pressed");
+  setTimeout(function() {
+    activeButton.classList.remove("pressed");
+  }, 100);
+
+}
